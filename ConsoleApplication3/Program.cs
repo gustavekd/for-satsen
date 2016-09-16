fba3da0a348ac7d8956e3b8b30d77055cd231274
gustavekd <gustav.ekelund99@gmail.com>
2016-09-16 11:53:16 +0200

    s

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -10,6 +10,29 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
+            //övning1
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("");
+
+            //övning2
+            for (int a = 20; a > 0; a--)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.WriteLine("");
+
+            //övning3
+
+            for (int b = 0; b <= 50; b = b + 2)
+            {
+                Console.WriteLine(b);
+            }
+            
         }
     }
 }
